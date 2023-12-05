072338c6d38555f64d8b17d930ea3ff2b4655554
LindaLor028 <77794383+LindaLor028@users.noreply.github.com>
2023-12-05 16:57:28 -0600

    Updated sparkles to be default value of 0

--- loginscreen/accounts/models.py
+++ loginscreen/accounts/models.py
@@ -11,7 +11,7 @@ class Profile(models.Model):
 @receiver(post_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
-        Profile.objects.create(user=instance)
+        Profile.objects.create(user=instance, sparkles=0)
 
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
