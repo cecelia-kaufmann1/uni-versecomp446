12d2c01a6c085e93b85cfd60fa89d11454149255
cecelia-kaufmann1 <78388297+cecelia-kaufmann1@users.noreply.github.com>
2023-12-11 12:57:29 -0600

    logout screen made, still need to work on aesthetic

--- loginscreen/loginscreen/settings.py
+++ loginscreen/loginscreen/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_REDIRECT_URL = "home"
-LOGOUT_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "logout"
 
 EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
 EMAIL_FILE_PATH = BASE_DIR / "sent_emails"
